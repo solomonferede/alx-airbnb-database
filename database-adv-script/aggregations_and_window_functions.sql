eab9cbef7198e9407c9da12e6c4c1bd7b11531b2
Solomon <solomonf1227@gmail.com>
2025-10-28 15:31:42 +0300

    Add aggregations_and_window_functions.sql with queries for total bookings and property ranking

---
+++ database-adv-script/aggregations_and_window_functions.sql
@@ -0,0 +1,24 @@
+-- 1️⃣ Total number of bookings made by each user
+SELECT 
+    u.user_id,
+    u.first_name,
+    u.last_name,
+    COUNT(b.booking_id) AS total_bookings
+FROM users u
+LEFT JOIN booking b ON u.user_id = b.user_id
+GROUP BY u.user_id, u.first_name, u.last_name
+ORDER BY total_bookings DESC;
+
+
+-- 2️⃣ Rank properties by number of bookings (Window Function)
+SELECT 
+    p.property_id,
+    p.name AS property_name,
+    COUNT(b.booking_id) AS total_bookings,
+    RANK() OVER (
+        ORDER BY COUNT(b.booking_id) DESC
+    ) AS property_rank
+FROM property p
+LEFT JOIN booking b USING(property_id)
+GROUP BY p.property_id, p.name
+ORDER BY property_rank;
